4db1556ab5f80483e9daa76157e007bbd1428ff0
Sofia W <sofiawberg@live.se>
2022-04-10 15:15:49 +0200

    test still works. removed commented out code in price-calculator

--- MAG-take-home-test/refactor/price-calculator.js
+++ MAG-take-home-test/refactor/price-calculator.js
@@ -36,39 +36,3 @@ const calculatePrice = (userType, productType, price, publishedDate) => {
   const finalPrice = price + productTypeSum - userTypeSum - publishedTodaySum;
   return finalPrice;
 };
-
-// var calculatePrice = function (userType, productType, price, publishedDate) {
-//   try {
-//     switch (userType) {
-//       case 0: // normal
-//         if (productType == 0) {
-//           // new product
-//           var enddateDiscount = 0;
-//           if (publishedDate.toDateString() == new Date().toDateString())
-//             enddateDiscount = 10;
-
-//           return price + 25 - enddateDiscount;
-//         } else if (productType == 1) {
-//           // old product
-//           return price + 35 - 0;
-//         }
-//         break;
-//       case 1: // company
-//         if (productType == 0) {
-//           // new product
-//           if (publishedDate.toDateString() === new Date().toDateString()) {
-//             return price + 25 - 15; // Enddate discount and company discount
-//           }
-
-//           return price + 25 - 5; // Only company discount
-//         } else if (productType == 1) {
-//           // old product
-//           return price + 35 - 5;
-//         }
-//         break;
-//     }
-//   } catch (ex) {
-//     console.log(ex);
-//   }
-//   return 0;
-// };
